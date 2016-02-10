dff1986925f591094b7719438450c1acf5b21d61
Frederick Giasson <fred@fgiasson.com>
2016-02-10 08:21:44 -0500

    Update to the latest version of Lucene 4.x

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject clj-fst "0.1.0"
+(defproject clj-fst "0.1.1"
   :description "Finite State Transducers (FST) for Clojure"
   :url "https://github.com/structureddynamics/clj-fst"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.apache.lucene/lucene-core "4.10.2"]
-                 [org.apache.lucene/lucene-misc "4.10.2"]
+                 [org.apache.lucene/lucene-core "4.10.4"]
+                 [org.apache.lucene/lucene-misc "4.10.4"]
                  [lein-marginalia "0.8.0"]]
   :source-paths      ["src/clojure"]
   :java-source-paths ["src/java"]
